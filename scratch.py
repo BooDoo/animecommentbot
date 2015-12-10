f4e302b1c586176bd09bab6ddcb70f05466127c7
Joel McCoy <joelmccoy+github@gmail.com>
2015-12-09 20:48:22 -0500

    Starting to add-in Crunchyroll integration...

--- scratch.py
+++ scratch.py
@@ -19,10 +19,16 @@ from moviepy.video.VideoClip import *
 from moviepy.video.tools.subtitles import SubtitlesClip
 from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
 
+from crunchyroll.apis.meta import MetaApi
+
 ### GLOBALS
 verbose=True
 font_name='Open-Sans-Semibold'
 queue_separator='====='
+
+## For Crunchyroll:
+api = MetaApi()
+all_series = [series.name for series in api.list_anime_series()]
 
 ## REGULAR EXPRESIONS:
 # Throw out junk strings (sound effects, speaker labels...)
@@ -391,4 +397,51 @@ def make_comment(count=1, out_path="output", vid_file=None):
             image_path = "{0}/{1}_{2:03d}.png".format(out_path, label, n)
             imwrite(image_path, frame)
             queue.write(u"{0}{1}{2}\n".format(image_path, queue_separator, txt_line).encode('utf8', 'replace') )
+
+###############################################################
+#
+#
+# CrunchyRoll stuff!
+#
+#
+###############################################################
+
+def search_series(q):
+    """
+        Using `all_series` list of series names, fetch any series
+        containing 'q' in the name
+
+        Our search for `q` is case-insensitive
+        API search uses Crunchyroll's casing
+    """
+    matching_names = filter(lambda name: q.lower() in name.lower(), all_series)
+    matching_series = list(api.search_anime_series(name)[0] for name in matching_names)
+    return matching_series
+
+def get_random_series(count=1):
+    random_series = [api.search_anime_series(name)[0] for name in sample(all_series, count)]
+    return random_series
+
+def free_eps(series):
+    try:
+        return [ep for ep in api.list_media(series) if ep.free_available]
+    except:
+        error(u"Something has gone wrong finding free episodes...")
+        return None
+
+def get_stream(episode, quality="720p"):
+    resolution = int( re.sub(r"\D", "", quality) )
+
+    if resolution > 480 and api.is_premium("anime") is False:
+        error(u"Not authorized for premium Anime. Reducing to 480p")
+        quality = "480p"
+        resolution = 480
+
+    try:
+        vid_format, vid_quality = api.get_stream_formats(episode).get(quality)
+        stream = api.get_media_stream(episode, vid_format, vid_quality)
+        return stream
+    except:
+        error(u"Something has gone wrong getting media stream...")
+        return None
 
